2c5ba5a3ed51cd861d68886af160ab933d4c22d6
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-04-22 01:24:11 +0200

    Category Model, DbContext,1st Migration

---
+++ AMS.API/Model/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMS.API.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.EnableSensitiveDataLogging();
+                // ...
+            }
+
+
+            public DbSet<Category> categories { get; set; }
+        }
+    }
+
---
+++ AMS.API/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMS.API.Model
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Slug { get; set; }
+    }
+}
