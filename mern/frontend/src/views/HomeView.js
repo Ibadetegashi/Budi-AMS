f10c964b5017347f0a33ff410072cf88321ce049
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-05-13 01:05:33 +0200

    Fetch data from backend

--- mern/frontend/src/views/HomeView.js
+++ mern/frontend/src/views/HomeView.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import Product from '../components/Product';
-import data from '../data';
-
-export default function HomeView() {
-  return (
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+function HomeView() {
+   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => (
-          <Product key={product._id} product={product}></Product>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
 }
+export default HomeView;
