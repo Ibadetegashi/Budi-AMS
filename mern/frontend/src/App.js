3144c70f45e871a71318adc19f72fc5162fb917b
Elfete Totaj <et48436@ubt-uni.net>
2022-05-13 16:26:59 +0200

    Style navbar with bootstrap

--- mern/frontend/src/App.js
+++ mern/frontend/src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeView from './Views/HomeView';
-import ProductView from './Views/ProductView';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeView from "./Views/HomeView";
+import ProductView from "./Views/ProductView";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
-     <BrowserRouter>
-      <div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" varian="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Budi</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductView />} />
-            <Route path="/" element={<HomeView />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductView />} />
+              <Route path="/" element={<HomeView />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center" > All right reserved! </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- mern/frontend/src/data.js
+++ mern/frontend/src/data.js
@@ -1,5 +1,4 @@
 const data = {
-<<<<<<< HEAD
   products: [
     {
       name: 'Nike Slim shirt',
@@ -50,88 +49,5 @@ const data = {
       description: 'high quality product',
     },
   ],
-=======
-    products : [
-        {
-          //image size: 680px by 830px
-            _id: '1',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/redOnion.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-        {
-            _id: '2',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/tomato.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-        {
-            _id: '3',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/redOnion.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-        {
-            _id: '4',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/redOnion.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-        {
-            _id: '5',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/redOnion.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-        {
-            _id: '6',
-            slug:'red-onion',
-            name: "Red Onion",
-            category: "Vegetable",
-            image: './images/redOnion.jpg',
-            price: 120,
-            countInStock: 10,
-            brand: null,
-            raiting: 4.5,
-            numReviews: 10,
-            description: 'high quality'
-        },
-    ],
->>>>>>> 6514505bf4ac1929675eabe8b914d0180394e489
 };
 export default data;
