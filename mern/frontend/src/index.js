dc904d51fe78522bfced362539277d7ea1dcc71e
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-05-15 14:15:20 +0200

    addToCart using Context

--- mern/frontend/src/index.js
+++ mern/frontend/src/index.js
@@ -6,13 +6,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { StoreProvider } from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
     <App />
     </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
 
