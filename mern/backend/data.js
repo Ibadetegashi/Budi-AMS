c5055fa605bb013daf3515d1564dc240375cb5d7
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-05-17 00:19:06 +0200

    user sample created

--- mern/backend/data.js
+++ mern/backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Ibadete',
+      email: 'admin@ams.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Fatoni',
+      email: 'user@ams.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: '1',
