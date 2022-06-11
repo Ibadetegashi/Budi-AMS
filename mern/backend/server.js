4bb33088a4e21e5d2173e9314792af3628b9cbc4
Elfete Totaj <et48436@ubt-uni.net>
2022-06-11 16:50:10 +0200

    Implement paying with paypal Ps.We should have a real ClientId

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -19,40 +19,21 @@ dotenv.config();
    });
 
 const app = express();
-// test
-// app.get('/api/products', (req, res) => {
-//   res.send(data.products);
-// });
-
-// app.get('/api/products/slug/:slug', (req, res) => {
-//   const product = data.products.find((x) => x.slug === req.params.slug);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
-// app.get('/api/products/:id', (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 
 app.use('/api/users', userRouter);
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
