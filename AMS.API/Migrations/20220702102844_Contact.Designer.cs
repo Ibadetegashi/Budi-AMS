a52a797c717fe26b0f6e1aaaf1d182e3bac63aec
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-07-02 13:14:41 +0200

    Contact APIs

--- AMS.API/Migrations/20220702102844_Contact.Designer.cs
+++ AMS.API/Migrations/20220702102844_Contact.Designer.cs
@@ -3,14 +3,16 @@ using AMS.API.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AMS.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220702102844_Contact")]
+    partial class Contact
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
