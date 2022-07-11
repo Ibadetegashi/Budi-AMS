cda61181feaebcf7cc5f836321b6d08e31b285c7
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-07-11 16:20:43 +0200

    Slider

--- mern/frontend/src/App.js
+++ mern/frontend/src/App.js
@@ -81,6 +81,7 @@ function App() {
       >
         <ToastContainer position="bottom-center" limit={1} />
         <header>
+      
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
 
@@ -195,71 +196,10 @@ function App() {
           </Nav>
         </div>
         <main>
+       
+          
+ 
           <Container className="mt-3">
-            {/* <div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
-
-              <div class="carousel-indicators">
-                <button
-                  type="button"
-                  data-mdb-target="#carouselBasicExample"
-                  data-mdb-slide-to="0"
-                  class="active"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                ></button>
-                <button
-                  type="button"
-                  data-mdb-target="#carouselBasicExample"
-                  data-mdb-slide-to="1"
-                  aria-label="Slide 2"
-                ></button>
-                <button
-                  type="button"
-                  data-mdb-target="#carouselBasicExample"
-                  data-mdb-slide-to="2"
-                  aria-label="Slide 3"
-                ></button>
-              </div>
-
-
-              <div class="carousel-inner">
-
-                <div class="carousel-item active">
-                  <img src="https://akologic.info/wp-content/uploads/2018/05/2.jpg" class="d-block w-100" alt="Sunset Over the City" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </div>
-                </div>
-
-
-                <div class="carousel-item">
-                  <img src="http://phama.com.au/wp-content/uploads/2021/06/FoodTank_agriculturesubsidiesworldbankreport.jpg" class="d-block w-200" alt="Canyon at Nigh" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </div>
-                </div>
-
-
-                <div class="carousel-item">
-                  <img src="https://images.squarespace-cdn.com/content/v1/59a706d4f5e2319b70240ef9/1598971164694-F75Y7VUAHC6TLFJXLCWF/veggies.jpg" class="d-block w-100" alt="Cliff Above a Stormy Sea" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                  </div>
-                </div>
-              </div>
-
-              <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div> */}
             <Routes>
               <Route
                 path="/admin/product/:id"
