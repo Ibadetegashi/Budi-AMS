19eb542ddb091af1f0e51c13ce60640073beb7c8
Elfete Totaj <et48436@ubt-uni.net>
2022-07-12 23:02:45 +0200

    Google login API

--- mern/frontend/src/views/SigninView.js
+++ mern/frontend/src/views/SigninView.js
@@ -1,76 +1,118 @@
-import Axios from 'axios';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { Helmet } from 'react-helmet-async';
-import { useContext, useEffect, useState } from 'react';
-import { Store } from '../Store';
-import { toast } from 'react-toastify';
-import { getError } from '../utils';
+import Axios from "axios";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Helmet } from "react-helmet-async";
+import { useContext, useEffect, useState } from "react";
+import { Store } from "../Store";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
+import GoogleLogin from "react-google-login";
 
 export default function SigninView() {
-    const navigate = useNavigate();
-    const { search } = useLocation();
-    const redirectInUrl = new URLSearchParams(search).get('redirect');
-    const redirect = redirectInUrl ? redirectInUrl : '/';
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get("redirect");
+  const redirect = redirectInUrl ? redirectInUrl : "/";
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem('loginData')
+      ? JSON.parse(localStorage.getItem('loginData'))
+      : null
+  );
 
-    const { state, dispatch: ctxDispatch } = useContext(Store);
-    const { userInfo } = state;
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            const { data } = await Axios.post('/api/users/signin', {
-                email,
-                password,
-            });
-            ctxDispatch({ type: 'USER_SIGNIN', payload: data });
-            localStorage.setItem('userInfo', JSON.stringify(data));
-            navigate(redirect || '/');
-        } catch (err) {
-            toast.error(getError(err));
-        }
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        if (userInfo) {
-            navigate(redirect);
-        }
-    }, [navigate, redirect, userInfo]);
-    return (
-        <Container className="small-container">
-            <Helmet>
-                <title>Sign In</title>
-            </Helmet>
-            <h1 className="my-3">Sign In</h1>
-            <Form onSubmit={submitHandler}>
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <div className="mb-3">
-                    <Button type="submit">Sign In</Button>
-                </div>
-                <div className="mb-3">
-                    New customer?{' '}
-                    <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
-                </div>
-            </Form>
-        </Container>
-    );
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await Axios.post("/api/users/signin", {
+        email,
+        password,
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect || "/");
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+  const handleFailure = (result) => {
+    alert(result);
+  };
+  const handleLogin = async (googleData) => {
+    const res = await fetch('/api/google-login', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem('loginData', JSON.stringify(data));
+  };
+  const handleLogout = () => {
+    localStorage.removeItem('loginData');
+    setLoginData(null);
+  };
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      <h1 className="my-3">Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
+          {loginData ? (
+            <div>
+              <h3>You logged in as {loginData.email}</h3>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <GoogleLogin
+              clientId={"318217546269-au1ntv1vgqkcutnoii4u9vqicpjbllfl.apps.googleusercontent.com"}
+              buttonText="Log in with Google"
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              cookiePolicy={'single_host_origin'}
+            ></GoogleLogin>
+          )}
+        </div>
+        <div className="mb-3">
+          New customer?{" "}
+          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
 }
